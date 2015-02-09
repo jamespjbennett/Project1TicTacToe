ac9383f356d9c01aef97b9c93626b06cfea88c62
jamespjbennett <jamespjbennett@hotmail.com>
2015-02-09 21:23:01 +0000

    adjusted the show form so that there are now radio buttons to choose each array element. This is submitting correctly to the update action. I now need to create the actions in the edit/update method to implement a change in the array." "

--- app/controllers/ttts_controller.rb
+++ app/controllers/ttts_controller.rb
@@ -1,10 +1,11 @@
 class TttsController < ApplicationController
 
   def new
-    @newgame = Ttt.new 
+    @game = Ttt.new 
   end
 
   def show
+    @game = Ttt.find(params[:id])
     @showgrid = Ttt.split_grid
   end
 

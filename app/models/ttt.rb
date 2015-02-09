4d5aa59af9151321b5da17ed4af60eb2387a16e9
jamespjbennett <jamespjbennett@hotmail.com>
2015-02-09 12:34:06 +0000

    homepage routing to home model, login appearing via application layout

--- app/models/ttt.rb
+++ app/models/ttt.rb
@@ -1,5 +1,4 @@
 class Ttt < ActiveRecord::Base
   has_many :moves
-  belongs_to :player_one, class_name: 'User'
-  belongs_to :player_two, class_name: 'User'
+  belongs_to :user
 end
--- db/migrate/20150209110647_create_ttts.rb
+++ db/migrate/20150209110647_create_ttts.rb
@@ -3,6 +3,8 @@ class CreateTtts < ActiveRecord::Migration
     create_table :ttts do |t|
       t.integer :player_one
       t.integer :player_two
+      t.integer :player_one_id , :references => :user_id
+      t.integer :player_two_id , :references => :user_id
       t.integer :computer
 
       t.timestamps null: false

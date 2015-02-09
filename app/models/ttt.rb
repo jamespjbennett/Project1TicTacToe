f6dc98e43764615d936982838fd48bed52fed2ae
jamespjbennett <jamespjbennett@hotmail.com>
2015-02-09 14:25:35 +0000

    experimenting with a basic method in the ttt model whereby a user can add numbers to  an array. Will adjust this so that user is defined as a winner if they come up with a correct winning combination.

--- app/models/ttt.rb
+++ app/models/ttt.rb
@@ -2,4 +2,13 @@ class Ttt < ActiveRecord::Base
   has_many :moves
   belongs_to :player_one_id, class_name: 'User'
   belongs_to :player_two_id, class_name: 'User'
+
+  
+  
+  @player_one_sequence = []
+  
+  def self.add_number(x)
+    @player_one_sequence << x
+  end
+
 end

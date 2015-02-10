596e787843195c8b73cb38998f56af719875cbeb
jamespjbennett <jamespjbennett@hotmail.com>
2015-02-10 10:29:40 +0000

    experimented with matching one specific winning array to the users chosen array. This is working and redirecting when the winning array is matched. now need to test different orders of that array, before creating a condition for all winning lines

--- app/controllers/ttts_controller.rb
+++ app/controllers/ttts_controller.rb
@@ -13,10 +13,13 @@ class TttsController < ApplicationController
     @player_one_array = []
     Ttt.show_grid[params[:index].to_i] = 'x'
     Ttt.player_moves << params[:index].to_i
-
-    if (@w1 || @w2 || @w3 || @w4 || @w5 || @w6 || @w7 || @w8).include?()
     binding.pry
-    redirect_to ttt_path
+
+    if Ttt.player_moves == Ttt.winner 
+      redirect_to new_ttt_path  
+    else
+      redirect_to ttt_path
+    end
   end
 
 
--- app/models/ttt.rb
+++ app/models/ttt.rb
@@ -8,14 +8,18 @@ class Ttt < ActiveRecord::Base
   
   #set the winning lines
 
-  @w1 = 0, 1, 2
-  @w2 = 0, 3, 6
-  @w3 = 0, 4, 8
-  @w4 = 1, 4, 7
-  @w5 = 2, 5, 8
-  @w6 = 2, 4, 6
-  @w7 = 3, 4, 5
-  @w8 = 6, 7, 8
+  @w1 = [0, 1, 2]
+  @w2 = [0, 3, 6]
+  @w3 = [0, 4, 8]
+  @w4 = [1, 4, 7]
+  @w5 = [2, 5, 8]
+  @w6 = [2, 4, 6]
+  @w7 = [3, 4, 5]
+  @w8 = [6, 7, 8]
+
+  def self.winner
+    @w1
+  end
    
   def self.show_grid
     @tttgrid

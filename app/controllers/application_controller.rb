2b76eaa018a7ea02bf1c7bbc9c1cacec1abdec90
jamespjbennett <jamespjbennett@hotmail.com>
2015-02-11 08:49:33 +0000

    adjusted the board code so when it is updated with x or 0, the user can no longer click on it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # resource == current_user
-    ttt_path(Ttt.last)
+    root_path
   end
 
   def after_sign_out_path_for(resource)
